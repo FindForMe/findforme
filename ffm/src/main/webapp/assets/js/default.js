d92ed526c96b8c73ec43ba3e12c4aab05e0df623
mahmad <mahamd@trackntell.cok>
2018-03-01 10:31:22 +0530

    rebaseing

--- ffm/src/main/webapp/assets/js/default.js
+++ ffm/src/main/webapp/assets/js/default.js
@@ -25,6 +25,18 @@ $('document').ready(function(){
 	break;
 	}
 	
+	
+	
+	// for handling CSRF token
+	var token = $('meta[name="_csrf"]').attr('content');
+	var header = $('meta[name="_csrf_header"]').attr('content');
+	
+	if((token!=undefined && header !=undefined) && (token.length > 0 && header.length > 0)) {		
+		// set the token header for the ajax request
+		$(document).ajaxSend(function(e, xhr, options) {			
+			xhr.setRequestHeader(header,token);			
+		});				
+	}
 	//var signin = $('#default-signin-modal').html();
 	//$('.default-signin').html(signin);
 	//$('#default-signin .default-pass').remove('.glyphicon-eye-close');
